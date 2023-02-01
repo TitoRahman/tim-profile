b23ba84d6414eb6852ff4f60433a1171d7eb06a8
Tito Ardimam Rahman <48154253+TitoRahman@users.noreply.github.com>
2023-02-01 15:54:41 +0700

    added container for About Me, Name, and Role

--- src/pages/components/MemberProfile.jsx
+++ src/pages/components/MemberProfile.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 // STOCK IMAGE : https://as2.ftcdn.net/v2/jpg/02/14/74/61/1000_F_214746128_31JkeaP6rU0NzzzdFC4khGkmqc8noe6h.jpg
 export default function MemberProfile({
-  memberPhotos = 'https://as2.ftcdn.net/v2/jpg/02/14/74/61/1000_F_214746128_31JkeaP6rU0NzzzdFC4khGkmqc8noe6h.jpg'
+  MemberPhoto = 'https://as2.ftcdn.net/v2/jpg/02/14/74/61/1000_F_214746128_31JkeaP6rU0NzzzdFC4khGkmqc8noe6h.jpg',
+  MemberAboutMe = 'about me!',
+  MemberName = 'My Name!',
+  MemberRole = 'My Role!'
 }) {
   // const skillTag = {
   //   FrontEnd : 
@@ -10,16 +13,21 @@ export default function MemberProfile({
   const responsiveSize = "lg:w-80 lg:h-80 sm:w-72 sm:h-72 h-72 w-48"
   return (
     <div className='flex flex-col items-center hover:animate-pulse hover:cursor-pointer'>
-      <div className={`rounded-full border overflow-hidden items-center flex flex-row mb-10 ${responsiveSize}`}>
-        <img alt='Member' src={memberPhotos} className={` ${responsiveSize} object-cover object-center`}></img>
+      {/* PROFILE IMAGE */}
+      <div className={`rounded-full border overflow-hidden items-center flex flex-row mb-3 ${responsiveSize}`}>
+        <img alt='Member' src={MemberPhoto} className={` ${responsiveSize} object-cover object-center`}></img>
       </div>
+      <div className='font-bold text-2xl'>
+        {MemberName.toUpperCase()}
+      </div>
+      <div className='text-gray-400 text-lg'>
+        {MemberRole.toUpperCase()}
+      </div>
+
       <div>
-        ROLE
+        {MemberAboutMe.toLowerCase()}
       </div>
-      <div>
-        ABOUT  ME
-      </div>
-      
+
       <div>
         SKILL
       </div>
