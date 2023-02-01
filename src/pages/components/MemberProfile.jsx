734a4b1b86c915722136ef6fde1ed80ebc43efc4
Tito Ardimam Rahman <48154253+TitoRahman@users.noreply.github.com>
2023-02-01 15:45:59 +0700

    Added Member profile container

--- src/pages/components/MemberProfile.jsx
+++ src/pages/components/MemberProfile.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+// STOCK IMAGE : https://as2.ftcdn.net/v2/jpg/02/14/74/61/1000_F_214746128_31JkeaP6rU0NzzzdFC4khGkmqc8noe6h.jpg
+export default function MemberProfile({
+  memberPhotos = 'https://as2.ftcdn.net/v2/jpg/02/14/74/61/1000_F_214746128_31JkeaP6rU0NzzzdFC4khGkmqc8noe6h.jpg'
+}) {
+  // const skillTag = {
+  //   FrontEnd : 
+  // }
 
-export default function MemberProfile() {
+  const responsiveSize = "lg:w-80 lg:h-80 sm:w-72 sm:h-72 h-72 w-48"
   return (
-    <div>MemberProfile</div>
+    <div className='flex flex-col items-center hover:animate-pulse hover:cursor-pointer'>
+      <div className={`rounded-full border overflow-hidden items-center flex flex-row mb-10 ${responsiveSize}`}>
+        <img alt='Member' src={memberPhotos} className={` ${responsiveSize} object-cover object-center`}></img>
+      </div>
+      <div>
+        ROLE
+      </div>
+      <div>
+        ABOUT  ME
+      </div>
+      
+      <div>
+        SKILL
+      </div>
+    </div>
   )
 }
