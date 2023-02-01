378e9b909cfb505590b22dc68bd52901c962d18d
Tito Ardimam Rahman <48154253+TitoRahman@users.noreply.github.com>
2023-02-01 14:29:27 +0700

    Added container for new component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import MemberProfile from './components/MemberProfile'
 
 export default function About() {
+  const titleStyle = 'text-white font-bold text-5xl mt-10 pb-1'
   return (
-    <div>About</div>
+    <div className='bg-main-dark pt-20 text-white flex flex-col gap-5 pb-96'>
+      <div>
+        <div className={titleStyle}>
+          OUR MEMBER
+        </div>
+        <div className='mt-3 grid grid-cols-3'>
+          <MemberProfile/>
+          <MemberProfile/>
+          <MemberProfile/>
+        </div>
+      </div>
+      <div className={titleStyle}>
+        WE HAVE ACHIEVED
+      </div>
+      <div className={titleStyle}>
+        BE OUR PARTNER
+      </div>
+    </div>
   )
 }
