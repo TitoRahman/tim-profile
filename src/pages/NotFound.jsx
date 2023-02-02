55c67d11c75c61f209dd8749bad5347f92dda8f0
Arif Jagad Mulya <arifjagad34@gmail.com>
2023-02-02 21:24:21 +0700

    Created 404 pages

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "flowbite-react";
 
-class NotFound extends Component {
-  render() {
-    return <div>NotFound</div>;
-  }
+export default function NotFound() {
+  return (
+    <div className="layout bg-main-dark h-auto lg:h-screen py-60 lg:my-0">
+      <div className="lg:mt-60 text-5xl text-white">Page Not Found</div>
+      <Link to="/">
+        <Button
+          color="white"
+          className="mt-8 border-2 border-white text-white w-40 button-slider"
+        >
+          Back to Home
+        </Button>
+      </Link>
+    </div>
+  );
 }
-
-export default NotFound;
